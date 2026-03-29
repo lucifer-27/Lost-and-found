b06ca4c05ebb16ae7ecc8a8a652ef11caed76d5b
vivanpandya <vivan.pandya15@gmail.com>
2026-03-29 20:04:56 +0530

    Changes in OTP System

--- app.py
+++ app.py
@@ -444,8 +444,12 @@ def send_otp_email(to_email, otp):
     sender_email = os.environ.get("EMAIL")
     sender_password = os.environ.get("EMAIL_PASS")
 
+    if not sender_email or not sender_password:
+        print("EMAIL CONFIG MISSING")
+        return
+
     msg = MIMEText(f"Your OTP is: {otp}")
-    msg["Subject"] = "CampusFind Password Reset OTP"
+    msg["Subject"] = "CampusFind OTP"
     msg["From"] = sender_email
     msg["To"] = to_email
 
@@ -455,9 +459,9 @@ def send_otp_email(to_email, otp):
         server.login(sender_email, sender_password)
         server.send_message(msg)
         server.quit()
-        print("OTP sent!")
+        print("OTP sent successfully ✅")
     except Exception as e:
-        print("Error:", e)
+        print("EMAIL ERROR:", e)
 
 # ---------------- HOME ----------------
 @app.route("/")
